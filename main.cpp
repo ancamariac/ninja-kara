e2ca2384d2e3aa809a6948f983b9db7e26cd288d
ancamariac <ciuciuancamaria@gmail.com>
2020-07-11 17:07:47 +0200

    Added livescore and sped up the platforms

--- main.cpp
+++ main.cpp
@@ -2,11 +2,21 @@
 #include <time.h>
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace sf;
 
 int main() {
 
 	srand(time(0));
+
+	std::ifstream fin;
+	fin.open("highscore.txt");
+	int highScore = 0;
+	fin >> highScore;
+	fin.close();
+
+	std::ofstream fout;
+	fout.open("highscore.txt", std::ios::trunc);
 
 	RenderWindow app(VideoMode(400, 533), "Ninja Kara");
 	app.setFramerateLimit(60);
@@ -18,14 +28,14 @@ int main() {
 
 	Sprite sBackground(t1), sPlat(t2), sPers(t3);
 
-	Text text, textScore;
+	Text text, textScore, liveScore, maxScore;
 	Font font;
 
 	if (!font.loadFromFile("assets/font.otf")) exit(0);
 	text.setCharacterSize(60);
 	text.setFillColor(sf::Color::Red);
 	text.setFont(font);
-	text.move(50, 100);
+	text.move(35, 100);
 	text.setOutlineColor(sf::Color::Black);
 	text.setOutlineThickness(3);
 
@@ -36,7 +46,22 @@ int main() {
 	textScore.setOutlineColor(sf::Color::Black);
 	textScore.setOutlineThickness(3);
 
-	sf::Vector2i plat[20];
+	liveScore.setCharacterSize(20);
+	liveScore.setFillColor(sf::Color::Red);
+	liveScore.setFont(font);
+	liveScore.move(10, 10);
+	liveScore.setOutlineColor(sf::Color::Black);
+	liveScore.setOutlineThickness(3);
+
+	maxScore.setCharacterSize(20);
+	maxScore.setFillColor(sf::Color::Red);
+	maxScore.setFont(font);
+	maxScore.move(360, 10);
+	maxScore.setOutlineColor(sf::Color::Black);
+	maxScore.setOutlineThickness(3);
+	maxScore.setString(std::to_string(highScore));
+
+	Vector2i plat[20];
 
 	// schDir == 0 => nu trebuie schimbat nimic, 1 == spre dreapta, -1 == spre stanga
 	// lr == LeftRight == directia spre care se uita Kara
@@ -44,7 +69,7 @@ int main() {
 	int lr = -1;
 
 	// la inceput, se genereaza 5 platforme
-	for ( int i = 0; i < 5; i++ ) {
+	for (int i = 0; i < 5; i++) {
 		plat[i].x = rand() % 330 + 10;
 		plat[i].y = 100 * i + rand() % 53;
 	}
@@ -62,8 +87,9 @@ int main() {
 		Event e;
 		while (app.pollEvent(e)) {
 
-			if (e.type == Event::Closed)
+			if (e.type == Event::Closed) {
 				app.close();
+			}
 		}
 
 		if (gameOver == 0) {
@@ -97,9 +123,29 @@ int main() {
 		if (y > 500) {
 
 			dy = -10;
-			text.setString("GAME OVER\n  Final score:");
+
+			if (score >= highScore) {
+				text.setString(" GAME OVER\nNew highscore:");
+				highScore = score;
+				if (fout.is_open()) {
+					fout << highScore;
+					fout.close();
+				}
+			}
+			else {
+				text.setString(" GAME OVER\n   Final score:");
+				if (fout.is_open()) {
+					fout << highScore;
+					fout.close();
+				}
+			}
 			textScore.setString(std::to_string(score));
 			gameOver = 1;
+			// play a song
+		}
+
+		for (int i = 0; i < 5; i++) {
+			plat[i].y += 1;
 		}
 
 		if (y < h) {
@@ -109,10 +155,8 @@ int main() {
 				y = h;
 				plat[i].y = plat[i].y - dy;
 
-				if (plat[i].y > 533) {
-					plat[i].y = 0;
-					plat[i].x = rand() % 330 + 10;
-
+				if (plat[i].y > 534) {
+					plat[i].y = 0; plat[i].x = rand() % 330 + 10;
 					if (gameOver == 0) {
 						score++;
 					}
@@ -123,13 +167,15 @@ int main() {
 		if (gameOver == 0) {
 			for (int i = 0; i < 5; i++) {
 				if ((x + 50 > plat[i].x) && (x + 20 < plat[i].x + 68)
-					&& (y + 70 > plat[i].y) && (y + 70 < plat[i].y + 14) && (dy > 0)) {
+					&& (y + 70 > plat[i].y) && (y + 70 < plat[i].y + 14) && (dy > 0))
+				{
 					dy = -10;
+					// play meow jump sound
 				}
 			}
 		}
 
-
+		liveScore.setString(std::to_string(score));
 		// sPers.getTexture()->getSize().x * sPers.getScale().x == 80 pt dreapta, -80 pt stanga
 		float newX = x - ((lr - 1) / 2) * 80;
 		sPers.setPosition(newX, y);
@@ -145,16 +191,15 @@ int main() {
 
 		app.draw(sBackground);
 
-
-
-		for (int i = 0; i < 5; i++)
-		{
+		for (int i = 0; i < 5; i++) {
 			sPlat.setPosition(plat[i].x, plat[i].y);
 			app.draw(sPlat);
 		}
 
 		if (gameOver == 0) {
 			app.draw(sPers);
+			app.draw(liveScore);
+			app.draw(maxScore);
 		}
 		else {
 			app.draw(text);
